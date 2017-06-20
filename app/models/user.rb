61496370263c534f1f449bc42270ced3dab4e053
Chris Hakos <chrishakos@gmail.com>
2017-06-20 09:45:12 -0400

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  attr_accessor :password
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    return self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.password_is?(password) ? user : nil
+  end
+
+  def password_is? (password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
+end
---
+++ db/migrate/20170620134304_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.timestamps null: false
+    end
+  end
+end
