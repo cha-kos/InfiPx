e9c02003bf2eb61b1cc957f0b6cfc9b3d59589c5
Chris Hakos <chrishakos@gmail.com>
2017-06-23 09:13:08 -0400

    FIX ANOTHER RELATIONS SYNTAX ERROR

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,11 +1,11 @@
 class Follow < ActiveRecord::Base
 
-  belongs_to :user
+  belongs_to :user,
   class_name: :user,
   foreign_key: :id,
   primary_key: :followee_id
 
-  has_many :users
+  has_many :users,
   class_name: :user,
   foreign_key: :follower_id,
   primary_key: :id
