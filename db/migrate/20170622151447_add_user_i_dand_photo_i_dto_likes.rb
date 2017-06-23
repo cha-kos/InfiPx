8e827e9fcfdd53bd8b027d6f7cfdae0d3a7a3748
Chris Hakos <chrishakos@gmail.com>
2017-06-22 20:32:36 -0400

    remove extra columns on like table

--- db/migrate/20170622151447_add_user_i_dand_photo_i_dto_likes.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIDandPhotoIDtoLikes < ActiveRecord::Migration
-  def change
-    add_column :likes, :user_id, :integer, null: false
-    add_column :likes, :photo_id, :integer, null: false
-  end
-end
