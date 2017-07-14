4b1f1c39406696f829b11df7f44eec57e7f86634
Chris Hakos <chrishakos@gmail.com>
2017-07-14 11:35:40 -0400

    add s3 protocol

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,13 @@ module InstaClone
     # config.i18n.default_locale = :de
     config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV["s3_bucket"],
       :access_key_id => ENV["s3_access_key_id"],
       :secret_access_key => ENV["s3_secret_access_key"],
       :s3_region => ENV['s3_region'],
       :s3_host_name => "s3-#{ENV["s3_region"]}.amazonaws.com",
-      :s3_protocol => :https
     }
   }
 
