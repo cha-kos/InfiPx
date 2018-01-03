b483ba4321ab2d15c1a72ed9e93aa487dd41fe5c
Chris Hakos <chrishakos@gmail.com>
2018-01-02 23:00:45 -0500

    create user_api_util

---
+++ frontend/util/user_api_util.js
@@ -0,0 +1,43 @@
+export const updateUser = ( user ) => {
+
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/users/${user.id}`,
+    data: {user: user},
+  });
+};
+
+export const updateUserAvatar = ( user ) => {
+
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/users/${user.id}`,
+    contentType: false,
+    processData: false,
+    data: user,
+  });
+};
+
+export const getUser = ( id ) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/users/${id}`
+  });
+};
+
+export const addFollow = ( follow ) => {
+  return $.ajax({
+    method: 'POST',
+    url: `api/follows`,
+    data: {follow: follow}
+  });
+};
+
+export const deleteFollow = ( follow ) => {
+
+  return $.ajax({
+    method: 'DELETE',
+    url: `api/follows/${follow.id}`,
+    data: { follow }
+  });
+};
